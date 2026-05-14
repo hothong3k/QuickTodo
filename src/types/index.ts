d3ced90231608359bf7a055b9101ed26a521f123
BAKKAA3K ThomasCoding <homaiduythong18106@gmail.com>
2026-05-14 23:26:59 +0700

    ✨ feat(todo): add todo description type and length constants

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,12 @@
 export interface Todo {
   id: string
   title: string
+  description?: string
   isDone: boolean
   priority: number
   createdAt: Date
 }
+
+export const TODO_TITLE_MAX_LENGTH = 100
+export const TODO_DESCRIPTION_MAX_LENGTH = 1000
+export const TODO_DESCRIPTION_PREVIEW_LENGTH = 100
